701addd2cb2a15b6326ef96f81107e3a542c8dd5
tgmangolini <taniagmangolini@gmail.com>
2018-10-07 20:47:34 -0300

    adjusts for deploy on openshift

---
+++ server.js
@@ -0,0 +1,31 @@
+// server.js
+
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
+var ip = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '0.0.0.0';
+
+app.set('port', port);
+
+//var server = http.createServer(myServerFunction);
+const server = http.createServer(app);
+
+server.listen(port, ip);
+console.log('API running on localhost:'+ port);
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -28,7 +28,6 @@ export class HomeComponent implements OnInit {
   hasResult: boolean = false;
   errorMsg = "";
 
-
   constructor(
     private modal: DialogService, 
     private spinnerService: Ng4LoadingSpinnerService,
